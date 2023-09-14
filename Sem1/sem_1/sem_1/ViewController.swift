7881d78a85c0c80aa07df8f0e8666f7a58a889f9
Balandzin_Anton <balandzin2602@gmail.com>
2023-09-14 20:24:09 +0300

    add ViewController2 + scene

--- Sem1/sem_1/sem_1/ViewController.swift
+++ Sem1/sem_1/sem_1/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     }()
     
     private var button: UIButton = {
-       var button = UIButton()
+        var button = UIButton()
         button.setTitle("Tap!", for: .normal)
         button.setTitle("Tap action", for: .highlighted)
         button.backgroundColor = .green
@@ -28,11 +28,13 @@ class ViewController: UIViewController {
         
     }()
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
         setupViews()
+        
+        button.addTarget(self, action: #selector(tap), for: .touchUpInside)
     }
     
     func setupViews() {
@@ -49,25 +51,29 @@ class ViewController: UIViewController {
             label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor
                                        , constant: 20),
-//            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 60),
-//            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -60),
+            //            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 60),
+            //            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -60),
             label.widthAnchor.constraint(equalToConstant: view.frame.width/1.5),
             label.heightAnchor.constraint(equalToConstant: view.frame.width/4),
-            
             //label.heightAnchor.constraint(equalToConstant: 100),
             
             button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 15),
-//            button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-//            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+            //            button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            //            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
             button.widthAnchor.constraint(equalToConstant: view
                 .frame.size.width/1.1),
             button.heightAnchor.constraint(equalToConstant: view
                 .frame.size.height/25)
-        
-        
+            
+            
         ])
     }
+}
 
+private extension ViewController {
+    @objc func tap() {
+        navigationController?.pushViewController(ViewController2(), animated: true)
+    }
 }
 
---
+++ Sem1/sem_1/sem_1/ViewController2.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController2.swift
+//  sem_1
+//
+//  Created by Антон Баландин on 14.09.23.
+//
+
+import UIKit
+
+final class ViewController2: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
+
+
