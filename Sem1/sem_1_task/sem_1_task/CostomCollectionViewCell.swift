0d77114b5187df311e1c6fbe14162aaaa6c09e6e
Balandzin_Anton <balandzin2602@gmail.com>
2023-09-16 18:56:35 +0300

    add CollectionViewController

---
+++ Sem1/sem_1_task/sem_1_task/CostomCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CostomCollectionViewCell.swift
+//  sem_1_task
+//
+//  Created by Антон Баландин on 16.09.23.
+//
+
+import UIKit
+
+class CostomCollectionViewCell: UICollectionViewCell {
+    
+}
