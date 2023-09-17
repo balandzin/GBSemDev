59015ca8251b8319cff3216d37beb0faa01eedbb
Balandzin_Anton <balandzin2602@gmail.com>
2023-09-17 18:43:37 +0300

    CostomCollectionViewCell

--- Sem1/sem_1_task/sem_1_task/CostomCollectionViewCell.swift
+++ Sem1/sem_1_task/sem_1_task/CostomCollectionViewCell.swift
@@ -8,5 +8,25 @@
 import UIKit
 
 class CostomCollectionViewCell: UICollectionViewCell {
+    var tap: ((UIImage) -> Void)?
+    private var imageView = UIImageView(image: UIImage (systemName: "person" ))
+    
+    override init (frame: CGRect) {
+    super.init (frame: frame)
+    setupViews ()
+    let recognizer = UITapGestureRecognizer(target: self, action: #selector (cellTap))
+    addGestureRecognizer(recognizer)
+    }
+    
+    required init? (coder: NSCoder) {
+    fatalError ("init(coder:) has not been implemented" )
+    }
+    
+    private func setupViews () {
+    addSubview(imageView)
+    setupConstraints ()
+    }
+    
+    
     
 }
