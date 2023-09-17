9d75d6f3a6110607629401de5d54c4963bb609fb
Balandzin_Anton <balandzin2602@gmail.com>
2023-09-17 18:46:26 +0300

    setupConstraints

--- Sem1/sem_1_task/sem_1_task/CostomCollectionViewCell.swift
+++ Sem1/sem_1_task/sem_1_task/CostomCollectionViewCell.swift
@@ -12,21 +12,35 @@ class CostomCollectionViewCell: UICollectionViewCell {
     private var imageView = UIImageView(image: UIImage (systemName: "person" ))
     
     override init (frame: CGRect) {
-    super.init (frame: frame)
-    setupViews ()
-    let recognizer = UITapGestureRecognizer(target: self, action: #selector (cellTap))
-    addGestureRecognizer(recognizer)
+        super.init (frame: frame)
+        setupViews ()
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector (cellTap))
+        addGestureRecognizer(recognizer)
     }
     
     required init? (coder: NSCoder) {
-    fatalError ("init(coder:) has not been implemented" )
+        fatalError ("init(coder:) has not been implemented" )
     }
     
     private func setupViews () {
-    addSubview(imageView)
-    setupConstraints ()
+        addSubview(imageView)
+        setupConstraints ()
     }
     
     
+    private func setupConstraints () {
+        imageView.translatesAutoresizingMaskIntoConstraints=false
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint (equalTo: topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    @objc func cellTap() {
+        tap? (imageView.image ?? UIImage ())
+    }
     
 }
