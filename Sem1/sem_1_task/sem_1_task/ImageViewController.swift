2edbef33bb43fdd981d155c0f4a1b581b69391bf
Balandzin_Anton <balandzin2602@gmail.com>
2023-09-17 18:53:32 +0300

    setupConstraints for ImageViewController

--- Sem1/sem_1_task/sem_1_task/ImageViewController.swift
+++ Sem1/sem_1_task/sem_1_task/ImageViewController.swift
@@ -7,14 +7,38 @@
 
 import UIKit
 
-class ImageViewControllerImageViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class ImageViewController: UIViewController {
+    
+    private var imageView = UIImageView()
+    
+    init (image: UIImage) {
+        super.init (nibName: nil, bundle: nil)
+        imageView.image = image
     }
     
-
-
+    required init?(coder: NSCoder) {
+        fatalError ("init (coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad ()
+        view.backgroundColor = .white
+        setupViews()
+    }
+    
+    private func setupViews () {
+        view.addSubview(imageView)
+        setupConstraints()
+    }
+    
+    private func setupConstraints () {
+        imageView.translatesAutoresizingMaskIntoConstraints=false
+        
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: view.frame.size.width/2),
+            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor)
+        ])
+    }
 }
