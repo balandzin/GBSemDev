85372c96cb85945f056e09d89a6c5e30192817f9
Balandzin_Anton <balandzin2602@gmail.com>
2023-09-17 18:48:28 +0300

    add ImageViewController

---
+++ Sem1/sem_1_task/sem_1_task/ImageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ImageViewController.swift
+//  sem_1_task
+//
+//  Created by Антон Баландин on 17.09.23.
+//
+
+import UIKit
+
+class ImageViewControllerImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
